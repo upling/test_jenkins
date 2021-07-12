79242f1f0236e9df4570fd98e6d1fe15c85a56bd
upling <upling.li@bindo.com>
2021-07-13 00:04:42 +0800

    [Add] 添加文件

---
+++ main.go
@@ -0,0 +1,17 @@
+// main.go文件
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		rw.Write([]byte("你好"))
+	}) //设置访问的路由
+	err := http.ListenAndServe(":80", nil) //设置监听的端口
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
